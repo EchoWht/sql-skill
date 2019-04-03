f44905a73a60d78649360bd87d57baa681bdf24f
王昊天 <echowht@qq.com>
2019-04-03 15:34:57 +0800

    Update decode.sql

--- decode.sql
+++ decode.sql
@@ -1,6 +1,11 @@
--- 最简单的用法，我的理解 decode(如果,等于0,则,否则)
+-- 1.最简单的用法，我的理解 decode(如果,等于0,则,否则)
 
 --结果是 true
 select decode(0,0,'true','false') from dual;
 --结果是 false
 select decode(0,1,'true','false') from dual;
+
+-- 2.比较大小
+select decode(sign(4-1),1,'4大','1大') from dual;
+select decode(sign(4-1),-1,'4小','1小') from dual;
+
