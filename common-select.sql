7a484afec8196bdf4381dfb339aaec62c1423eef
王昊天 <echowht@qq.com>
2019-04-29 12:43:04 +0800

    Update common-select.sql

--- common-select.sql
+++ common-select.sql
@@ -1,2 +1,7 @@
 --查找某个字段重复的次数
 SELECT COUNT(*) FROM TABLE_NAME GROUP by FIELD_NAME  order by  COUNT(*) desc;
+
+
+--查询带有下划线特殊字符的数据
+
+select * from TABLE_NAME where FIELD_NAME like '\_%'  escape '\';
