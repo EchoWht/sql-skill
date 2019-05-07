6d1a21f65bccc60e26ec4dbba7127fc8c1a20293
王昊天 <echowht@qq.com>
2019-05-07 11:32:54 +0800

    Update decode.sql

--- decode.sql
+++ decode.sql
@@ -9,3 +9,8 @@ select decode(0,1,'true','false') from dual;
 select decode(sign(4-1),1,'4大','1大') from dual;
 select decode(sign(4-1),-1,'4小','1小') from dual;
 
+
+--3.其他场景中使用，例如两个字段相除
+select decode(b,0,'除数为0', ROUND(a/b,2)) from table_name
+
+
