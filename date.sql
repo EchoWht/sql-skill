95aa6e0a25d90504d9f247201737bf6c14ad3969
王昊天 <echowht@qq.com>
2019-05-18 16:03:21 +0800

    Update date.sql

--- date.sql
+++ date.sql
@@ -6,3 +6,11 @@ select add_months(trunc(sysdate),-1) from dual;
 
 --前一天
 select to_char(TRUNC(SYSDATE - 1) ,'YYYYMMDD')  from dual;
+
+--计算日期之间的天数，结果为3
+select to_date('2019-05-21 00:00:00' ,'yyyy-mm-dd hh24:mi:ss')-to_date('2019-05-18 00:00:00' ,'yyyy-mm-dd hh24:mi:ss') from dual;
+
+--计算日期之间的小时数，结果为72
+select round((cast(to_date('2019-05-21 00:00:00', 'yyyy-mm-dd hh24:mi:ss') as DATE) -
+              cast(to_date('2019-05-18 00:00:00', 'yyyy-mm-dd hh24:mi:ss') as DATE)) * 24) as diffHour
+from dual;
