a2bb9b459bf9a9410038b507add5b05e455d401e
王昊天 <echowht@qq.com>
2019-06-21 11:44:31 +0800

    Update common-select.sql

--- common-select.sql
+++ common-select.sql
@@ -5,3 +5,7 @@ SELECT COUNT(*) FROM TABLE_NAME GROUP by FIELD_NAME  order by  COUNT(*) desc;
 --查询带有下划线特殊字符的数据
 
 select * from TABLE_NAME where FIELD_NAME like '\_%'  escape '\';
+
+
+-- 查询某个字段重复的数据(不包括null)
+select user_code,USERNAME,REALNAME from COMMON_USER where user_code in(select user_code from COMMON_ group by user_code having  count(user_code)>1);
