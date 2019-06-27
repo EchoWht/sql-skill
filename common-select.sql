6455bc1ff08266b455d0af7aacc4dce59fa86159
王昊天 <echowht@qq.com>
2019-06-27 15:49:50 +0800

    Update common-select.sql

--- common-select.sql
+++ common-select.sql
@@ -9,3 +9,6 @@ select * from TABLE_NAME where FIELD_NAME like '\_%'  escape '\';
 
 -- 查询某个字段重复的数据(不包括null)
 select user_code,USERNAME,REALNAME from COMMON_USER where user_code in(select user_code from COMMON_ group by user_code having  count(user_code)>1);
+
+--特别注意加法时任何数字+null都为null
+select 1+null from dual;--结果为null
