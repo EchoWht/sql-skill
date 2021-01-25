a007dd4ebef0e1eb1f8b158be2c1595ef717816f
王昊天 <echowht@qq.com>
2021-01-25 14:06:31 +0800

    Update create_user_grant.sql

--- create_user_grant.sql
+++ create_user_grant.sql
@@ -1,2 +1,12 @@
+-- 创建用户
 CREATE USER  'newusername'@'%'  IDENTIFIED BY  'password';
+-- 赋某个数据库的权限
 grant all on databasename.* to 'newusername'@'%';
+
+
+
+
+-- 查询mysql的用户
+SELECT User,Host FROM mysql.user;
+-- 查询用户的权限
+show grants for 'username'@'%';
