d5eeabaf09a125d476119e1a6768222928a0dc4d
王昊天 <echowht@qq.com>
2021-01-29 10:58:00 +0800

    Update table.sql

--- table.sql
+++ table.sql
@@ -3,3 +3,8 @@ rename table sport_log to sport_log_2020_08_05;
 
 -- 复制表结构
 create table sport_log like sport_log_2020_08_05;
+
+-- 根据某个表创建表并插入数据
+create table new_table select field_a,field_b
+from old_table
+where field_a = 'xx'
